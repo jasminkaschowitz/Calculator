aa6bae6c676a9996a2463771c992de978a99be50
Jasmin Kaschowitz <jasmin.kaschowitz@gmail.com>
2021-10-08 19:59:48 +0200

    Variablen deklarieren

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,14 @@
 public class Calculator {
     public static void main(String[] args) {
-
+        int a = 1;
+        System.out.println(a);
+        double b = 5.5;
+        System.out.println(b);
+        boolean c = true;
+        System.out.println(c);
+        char d = 'j';
+        System.out.println(d);
+        String e = "Heute";
+        System.out.println(e);
     }
 }
