92fcd6b8e0cb54ced2126b9d8dbeb853f0e54adb
Jasmin Kaschowitz <jasmin.kaschowitz@gmail.com>
2021-10-08 20:23:38 +0200

    Calculations

---
+++ src/ArithmetischeOperationen.java
@@ -0,0 +1,18 @@
+public class ArithmetischeOperationen {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 6;
+        int div = a/b;
+
+        System.out.println(a + " + " + b + " = " + (a+b));
+        System.out.println(a + " - " + b + " = " + (a-b));
+        System.out.println(a + " * " + b + " = " + (a*b));
+        System.out.println(a + " / " + b + " = " + div);
+        System.out.println(a + " % " + b + " = " + (a%b));
+        //kann auch für Addition, Division etc. extra eine Variable vergeben
+
+
+
+
+    }
+}
