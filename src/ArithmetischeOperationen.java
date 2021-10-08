ff9125faa150fc28ce9d5d6281576cc07b9884cb
Jasmin Kaschowitz <jasmin.kaschowitz@gmail.com>
2021-10-08 21:32:37 +0200

    Methode

--- src/ArithmetischeOperationen.java
+++ src/ArithmetischeOperationen.java
@@ -2,17 +2,45 @@ public class ArithmetischeOperationen {
     public static void main(String[] args) {
         int a = 10;
         int b = 6;
-        int div = a/b;
+        int div = a / b;
 
-        System.out.println(a + " + " + b + " = " + (a+b));
-        System.out.println(a + " - " + b + " = " + (a-b));
-        System.out.println(a + " * " + b + " = " + (a*b));
+        System.out.println(a + " + " + b + " = " + (a + b));
+        System.out.println(a + " - " + b + " = " + (a - b));
+        System.out.println(a + " * " + b + " = " + (a * b));
         System.out.println(a + " / " + b + " = " + div);
-        System.out.println(a + " % " + b + " = " + (a%b));
+        System.out.println(a + " % " + b + " = " + (a % b));
         //kann auch für Addition, Division etc. extra eine Variable vergeben
 
+        float f = a / b;
+        System.out.println("float: " + f);
+        //zwei int Werte ergeben eine ganze Zahl
+
+        double g = a / (b * 0.1);
+        System.out.println("double: " + g);
+        //Zahl mal 0.1 multiplizieren
+        //float f = (float)...
+
+        boolean c = 2 == 2 && 3 == 3;
+        System.out.println(c);
 
 
+        int result = add(a,b);
+
+        printResult(result);
 
     }
+
+
+    public static void printResult(int result) {
+        System.out.println(result);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+
+    }
+
+
 }
+
+
--- src/Calculator.java
+++ src/Calculator.java
@@ -10,5 +10,39 @@ public class Calculator {
         System.out.println(d);
         String e = "Heute";
         System.out.println(e);
+
+        int resultAdd = add(5, 3);
+        int resultSub = sub(5, 3);
+        int resultMulti = multi(5,3);
+        int resultDiv = div(5, 3);
+        int resultRest = rest(5, 3);
+
+        System.out.println(resultAdd);
+        System.out.println(resultSub);
+        System.out.println(resultMulti);
+        System.out.println(resultDiv);
+        System.out.println(resultRest);
+
+
+
+
+
     }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+    public static int multi(int a, int b) {
+        return a * b;
+    }
+    public static int div(int a, int b) {
+        return a / b;
+    }
+    public static int rest(int a, int b) {
+        return a % b;
+    }
+
 }
